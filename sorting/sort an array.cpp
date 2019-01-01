c1d51dcaf184f8c41d25ac6a0d4c6c7680ab3d53
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-01-01 19:19:58 +0530

    Create sort an array.cpp

---
+++ sorting/sort an array.cpp
@@ -0,0 +1,22 @@
+#include<algorithm>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    vector<int> v(n);
+	    for(int i=0;i<n;i++)
+	    cin>>v[i];
+	    sort(v.begin(),v.end());
+	    for(int i=0;i<n;i++)
+	    cout<<v[i]<<" ";
+	    cout<<endl;
+	}
+
+	return 0;
+}
