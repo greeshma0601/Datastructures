cdea44a2e2566f5e3013082f99a8a55df39b54c8
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-01-01 17:44:09 +0530

    Create Selection sort.cpp

---
+++ Selection sort.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+int select(int arr[], int i);
+ 
+// A function to implement bubble sort
+void selectionSort(int arr[], int n)
+{
+   int i, j;
+   for (i = n-1; i >=0; i--)      
+   {
+        int j = select(arr, i);
+        swap(&arr[i], &arr[j]);
+   }
+}
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("
+");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    selectionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* The task is to complete select() which is used 
+   as shown below to implement selectionSort() */
+/* A function to implement selection sort
+void selectionSort(int arr[], int n)
+{
+   int i, j;
+   for (i = n-1; i >=0; i--)      
+   {
+        int j = select(arr, i);
+        swap(&arr[i], &arr[j]);
+   }
+}
+ */
+int select(int a[], int i)
+{
+    // Your code here
+    int k=0;
+    int min=i;
+    for(int j=0;j<i;j++)
+    {
+        if(a[j]>a[min])
+        min=j;
+    }                                                                                                
+    return min;
+}
