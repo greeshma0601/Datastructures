912bf048696e862773faa0dfc1d57bec2693286f
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-01-10 00:52:54 +0530

    Create Children Sum Parent.cpp

---
+++ Trees/Children Sum Parent.cpp
@@ -0,0 +1,145 @@
+/*
+Children Sum Parent
+Given a binary tree, write a function that returns true if the tree satisfies the following property.
+
+For every node, data value must be equal to the sum of data values in left and right children. Consider data value as 0 for NULL children.
+
+Note:The Input/Ouput format and Example given are used for system's internal purpose, and should be used by a user for Expected Output only. As it is a function problem, hence a user should not read any input from stdin/console. The task is to complete the function specified, and not to write the full code.
+
+Input:
+The first line consists of T test cases. The first line of every test case consists of N, denoting the number of edges in the tree. The second and third line of every test case consists of N, nodes of the binary tree.
+
+
+Output:
+Return 1 if the given tree satisfies the given property else return 0. 
+
+Constraints:
+1<=T<=100
+1<=N<=100
+
+Example:
+Input:
+2
+3
+4 1 L 4 3 R 3 3 L
+5
+10 8 L 10 2 R 8 3 L 8 5 R 2 2 Lâ€‹
+
+Output:
+1
+1
+
+
+input
+2
+3
+4 1 L 4 3 R 3 3 L
+5
+10 8 L 10 2 R 8 3 L 8 5 R 2 2 L
+*/
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+	int data;
+	Node *left,*right;
+};
+Node *newNode(int data)
+{
+    Node* temp = new Node;
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+ 
+    return (temp);
+}
+void insert(Node *root,int a1,int a2,char lr){
+	if(root==NULL){
+		return;
+	}
+	if(root->data==a1){
+		switch(lr){
+			case 'L':root->left=newNode(a2);
+			break;
+			case 'R':root->right=newNode(a2);
+			break;
+		}
+	}
+	else{
+		insert(root->left,a1,a2,lr);
+		insert(root->right,a1,a2,lr);
+	}
+}
+void inorder(Node *root){
+	if(root==NULL)
+		return;
+	inorder(root->left);
+	cout<<root->data<<" ";
+	inorder(root->right);
+}
+int isSumProperty(Node *node);
+int main()
+{
+	//freopen("input.txt","r",stdin);
+	//freopen("output.txt","w",stdout);
+	int t;
+	cin>>t;
+	while(t-->0)
+	{
+		int n;
+		cin>>n;
+		int m=n;
+		Node *root1=NULL;
+		while(n-->0){
+			int a1,a2;
+			cin>>a1>>a2;
+			char lr;
+			scanf(" %c",&lr);
+			if(root1==NULL){
+				root1=newNode(a1);
+				switch(lr){
+					case 'L':root1->left=newNode(a2);
+					break;
+					case 'R':root1->right=newNode(a2);
+					break;
+				}
+			}
+			else{
+				insert(root1,a1,a2,lr);
+			}
+		}
+			cout<<isSumProperty(root1)<<"
+";
+	}
+}
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/*Complete the function below
+Node is as follows:
+struct Node{
+	int data;
+	Node *left,*right;
+};
+*/
+int isSumProperty(Node *node)
+{
+ // Add your code here
+ int leftdata=0,rightdata=0;
+ if(node==NULL || (node->left==NULL && node->right==NULL))
+ return 1;
+ else
+ {
+     if(node->left)
+     leftdata=node->left->data;
+     if(node->right)
+     rightdata=node->right->data;
+     if((node->data==leftdata+rightdata)&& isSumProperty(node->left) && isSumProperty(node->right))
+     return 1;
+     else
+     return 0;
+ }
+}
