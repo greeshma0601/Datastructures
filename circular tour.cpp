0f227877c710dde6c3c3b55d4fc1cdd5d9a57cbd
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-01-12 10:28:25 +0530

    Update circular tour.cpp

--- circular tour.cpp
+++ circular tour.cpp
@@ -1,3 +1,36 @@
+/*
+Circular tour Submissions: 14721   Accuracy: 33.91%   Difficulty: Medium   Marks: 4
+           
+Suppose there is a circle. There are N petrol pumps on that circle. You will be given two sets of data.
+1. The amount of petrol that every petrol pump has.
+2. Distance from that petrol pump to the next petrol pump.
+
+Your task is to complete the function tour() which returns an integer denoting the first point from where a truck will be able to complete the circle (The truck will stop at each petrol pump and it has infinite capacity).
+
+Note :  Assume for 1 litre petrol, the truck can go 1 unit of distance.
+
+Input:
+The first line of input will be the number of test cases. Then T test cases follow. Each Test case contains 2 lines. The first line will contain an integer N denoting the number of petrol pumps and in the next line are N space separated values petrol and distance denoting the amount of petrol every petrol pump has and the distance to next petrol pump respectively .
+
+Output:
+The output of each test case will be the index of the the first point from where a truck will be able to complete the circle otherwise -1 .
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 50
+1 <= petrol, distance <= 100
+
+Example (To be used only for expected output)
+Input:
+1
+4
+4 6 6 5 7 3 4 5
+Output:
+1
+
+
+*/
+
 {
 #include<bits/stdc++.h>
 using namespace std;
