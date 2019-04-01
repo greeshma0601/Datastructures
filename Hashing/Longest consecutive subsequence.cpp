3705e285802761a8b0588ac85748220f992aa22b
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-04-01 23:52:45 +0530

    Update Longest consecutive subsequence.cpp

--- Hashing/Longest consecutive subsequence.cpp
+++ Hashing/Longest consecutive subsequence.cpp
@@ -1,4 +1,19 @@
 /*
+1) Create an empty hash.
+2) Insert all array elements to hash.
+3) Do following for every element arr[i]
+....a) Check if this element is the starting point of a 
+       subsequence.  To check this, we simply look for
+       arr[i] - 1 in hash, if not found, then this is
+       the first element a subsequence.  
+    
+       If this element is a first element, then count 
+       number of elements in the consecutive starting 
+       with this element.
+
+       If count is more than current res, then update    
+       res.
+
 Longest consecutive subsequence Submissions: 11111   Accuracy: 49.37%   Difficulty: Medium   Marks: 4
 Associated Course(s):   Geeks Classes DSA   Geeks Classes in Noida
         
