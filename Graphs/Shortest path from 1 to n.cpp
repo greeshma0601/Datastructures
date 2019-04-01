99f760d9df8c65f78525f417600e782b2c5fb6fb
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-04-01 12:38:03 +0530

    Update Shortest path from 1 to n.cpp

--- Graphs/Shortest path from 1 to n.cpp
+++ Graphs/Shortest path from 1 to n.cpp
@@ -1,4 +1,21 @@
 /*
+Let n be 100 for example:
+Since the task is to minimize number of edges, 
+we would prefer to follow 3*i.  
+
+Let us follow multiple of 3.
+
+1 => 3 => 9 => 27 => 81, now we can't follow multiple
+of 3. So we will have to follow i+1. This solution gives
+a long path.
+
+What if we begin from end, and we reduce by 1 if 
+the value is not multiple of 3, else we divide by 3.
+100 => 99 => 33 => 11 => 10 => 9 => 3 => 1
+
+So we need total 7 edges.
+
+
 Shortest path from 1 to n
 Consider a directed graph whose vertices are numbered from 1 to n. There is an edge from a vertex i to a vertex j iff either j = i + 1 or j = 3i. The task is to find the minimum number of edges in a path in G from vertex 1 to vertex n.
 
